51114880b6405ab38957a2a43c1f88a0847aab81
Franklyn.Roth <franklynroth@gmail.com>
2020-04-25 13:39:23 -0600

    Update code block

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ app.get('/', (req, res) => {
 res.json({
 	'message': 'This bot should be online! Uptimerobot will keep it alive',
 	'uptime robot': 'https://stats.uptimerobot.com/2G0OZTkVwB'
+	});
 });
 app.get("/", (request, response) => {
   response.sendStatus(200);
